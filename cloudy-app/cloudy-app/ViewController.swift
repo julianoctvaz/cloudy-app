3c93c04595abee0a664af5f37036c5486a50645e
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2020-06-16 17:04:30 -0300

    Cria projeto base no Xcode

---
+++ cloudy-app/cloudy-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cloudy-app
+//
+//  Created by José Henrique Fernandes Silva on 16/06/20.
+//  Copyright © 2020 sorvete-napolitano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
