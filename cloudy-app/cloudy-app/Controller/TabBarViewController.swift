e05e5a9944324c4249f9f358650968c8d09d0b68
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2020-07-01 20:38:29 -0300

    Cria tabBarController

---
+++ cloudy-app/cloudy-app/Controller/TabBarViewController.swift
@@ -0,0 +1,64 @@
+//
+//  TabBarViewController.swift
+//  cloudy-app
+//
+//  Created by Juliano Vaz on 01/07/20.
+//  Copyright © 2020 sorvete-napolitano. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+//
+//    override func viewDidAppear(_ animated: Bool) {
+//           gambiSetaVoceComUmaNotificacao()
+//    }
+    
+    //override func loadView() {
+     // gambiSetaVoceComUmaNotificacao()
+    //}
+//    override func tabBar(_ tabBar: UITabBar, didBeginCustomizing items: [UITabBarItem]) {
+//        print(items)
+//         if (items[2].title == "Voce"){
+//                    tabBarItem.badgeValue = "1"
+//        }
+//    }
+//     func gambiSetaVoceComUmaNotificacao() {
+//        if (tabBarItem.title == "Voce"){
+//            tabBarItem.badgeValue = "1"
+//
+//        if let tabTitles = self.tabBarController?.tabBar.items
+//            {
+//                tabTitles[0].badgeValue = "0"
+//                tabTitles[1].badgeValue = "3"
+//                 tabTitles[1].badgeValue = "1"
+//            }
+//        }
+//    }
+    
+            
+            
+        
+            
+       // .tabBarItem.badgeValue = "0"
+        
+//        azul.tabBarItem.badgeValue = "0"
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
